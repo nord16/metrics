c9ea173eb547b04e0ceaa37bf6d731a090aab9cb
nord16 <a.molchin@mgcom.ru>
2017-05-18 22:40:31 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+
+package main
+
+import (
+	"log"
+
+	metrika "github.com/xboston/go-yandex-metrika"
+)
+
+func main() {
+
+	log.Println("Start")
+
+	token := ""
+
+	metrika := metrika.NewMetrikaFromToken(token)
+	metrika.SetDebug(true)
+
+	counterList, _ := metrika.GetCounterList()
+
+	for _, counter := range counterList.Counters {
+		log.Println(counter.ID, counter.Name, counter.Site)
+	}
+
+	log.Println("Finish")
+}
